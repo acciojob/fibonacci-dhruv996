d69fa0f218480096832dea8c1fd7df5eafed5a06
Dhruv Varshney <144780592+dhruv996@users.noreply.github.com>
2023-12-21 15:05:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function fibonacci(num) {
-// your code here
+  if (num <= 0) {
+    return "Invalid input. Please enter a positive integer.";
+  }
+
+  let fibArr = [0, 1];
+
+  for (let i = 2; i <= num; i++) {
+    fibArr[i] = fibArr[i - 1] + fibArr[i - 2];
+  }
+
+  return fibArr[num - 1];
 }
 
-module.exports = fibonacci;
+function calculateFibonacci() {
+  const fibonacciInput = document.getElementById('fibonacciInput').value;
+  const num = parseInt(fibonacciInput);
+
+  const result = fibonacci(num);
+
+  document.getElementById('result').innerText = `Fibonacci(${num}): ${result}`;
+}
